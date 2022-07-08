d2c115f0ea4d8a3400ed2b88d1212d0661c5ac9b
Atsuki Hashizume <atk01322@gmail.com>
2022-07-08 19:20:17 +0000

    fix footer and add viewport setting

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,15 +2,19 @@
     <head>
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @component('components.header')
-        @endcomponent
-        <div class="container">
+         @endcomponent
+         <div class="container">
             @yield('content')
-        </div>
+         </div>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+         @component('components.footer')
+         @endcomponent
+
+         <script src="{{ mix('js/app.js') }}"></script>
     </body>
-</html>
+</html> 
